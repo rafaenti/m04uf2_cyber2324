819623746df72b03ca93c6dda11458b40c6de4ef
Rafa Laguna <rafa.laguna@enti.cat>
2024-04-18 09:28:41 +0200

    Python read values update

--- characters.py
+++ characters.py
@@ -9,8 +9,21 @@ facx = minidom.parse('characters.facx')
 characters = facx.getElementsByTagName('character')
 
 for character in characters:
+	id = character.getAttribute('id')
+
 	name = character.getElementsByTagName('name')
-	print(name[0].tagName)
+	name_value = name[0].firstChild.nodeValue
 
 	age = character.getElementsByTagName('age')
-	print(age[0].tagName)
+	age_value = age[0].firstChild.nodeValue
+
+	gender = character.getElementsByTagName('gender')
+	gender_value = gender[0].getAttribute('value')
+
+	level = character.getElementsByTagName('level')
+	level_value = level[0].getAttribute('value')
+
+	print(f"{id}\t {name_value}\t {age_value}\t {gender_value}\t {level_value}")
+
+
+	print("-"*64)
